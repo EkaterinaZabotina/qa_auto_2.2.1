e51043491c99457d8df1ec096c3290e4c3afcf40
Ekaterina <ekaterinaclassictravel@gmail.com>
2024-06-12 16:07:57 +0300

    added file with increment and decrement

--- calculations.js
+++ calculations.js
@@ -1,21 +1,56 @@
 function increaseBalance(x, y) {
-  x += y
-  return x;
+x = x + y;
+return x;
 }
 
 function decreaseBalance(x, y) {
-return x -= y;
+x = x - y;
+return x;
 }
 
 function divideBalanceByAccounts(balance, accNumber) {
-  return balance / accNumber;
-   }
-
-function getRestAfterDivision(x, y) {
-    return x %= y;
+let average = balance / accNumber;
+return average;
 }
 
+function getRestAfterDivision(x, y) {
+x = x % y;
+return x;
+}
+
+function getIncrementPost(x) {
+    return x++;
+}
+
+function getIncrementPre(x) {
+    return ++x;
+}
+
+function getIncrementPost(x) {
+    return x++;
+}
+
+function getIncrementPre(x) {
+    return ++x;
+}
+
+function getDecrementPost(x) {
+    return x--;
+}
+
+function getDecrementPre(x) {
+    return --x;
+}
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+console.log(getIncrementPost(3));
+console.log(getIncrementPre(3));
+console.log(getDecrementPost(3));
+console.log(getDecrementPre(3));
+
+
+let x = 3;
+const y = x++;
+console.log(x,y)
